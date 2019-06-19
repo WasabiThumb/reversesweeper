0df1e7d18d08d679304fbb56fd558e440612e45a
Nnnes <N@nnnes.net>
2019-06-18 22:32:34 -0500

    Eliminate some magic numbers
    Adds BOARD_SIZE constant.
    Also makes spaces around operators more consistent

--- reversesweeper.py
+++ reversesweeper.py
@@ -4,9 +4,11 @@ from pygame.time import Clock
 
 pygame.init()
 
-screen = pygame.display.set_mode((650, 750))
+BOARD_SIZE = 13
+
+screen = pygame.display.set_mode((BOARD_SIZE * 50,  BOARD_SIZE * 50 + 100))
 myimage = pygame.image.load("assets/board.png")
-imagerect = Rect(0,0,650,750)
+imagerect = Rect(0, 0, BOARD_SIZE * 50, BOARD_SIZE * 50 + 100)
 
 FPS = 60
 clock = pygame.time.Clock()
@@ -24,25 +26,25 @@ revealedList = []
 def clean():
     lines.clear()
     revealedList.clear()
-    for times in range(13):
+    for times in range(BOARD_SIZE):
         emptyTab = []
-        for timesx in range(13):
+        for timesx in range(BOARD_SIZE):
             emptyTab.append(0)
         revealedList.append(emptyTab)
-    for x in range(13):
+    for x in range(BOARD_SIZE):
         xarray = []
-        for y in range(13):
-            xarray.append(random.choice([10,10,10,10,9]))
+        for y in range(BOARD_SIZE):
+            xarray.append(random.choice([10, 10, 10, 10, 9]))
         lines.append(xarray)
     finalClean()
 
 def finalClean():
-    for l in range(13):
-        for z in range(13):
+    for l in range(BOARD_SIZE):
+        for z in range(BOARD_SIZE):
             minesNear = 0
             if lines[l][z] == 10:
-                for r in range(max(0, l - 1), min(12, l + 1) + 1):
-                    for c in range(max(0, z - 1), min(12, z + 1) + 1):
+                for r in range(max(0, l - 1), min(BOARD_SIZE - 1, l + 1) + 1):
+                    for c in range(max(0, z - 1), min(BOARD_SIZE - 1, z + 1) + 1):
                         if lines[r][c] == 9: minesNear += 1
                 lines[l][z] = 8 - minesNear
 
@@ -50,8 +52,8 @@ clean()
 
 def gameEnd():
     endAtRenderStop = True
-    for revX in range(13):
-        for revY in range(13):
+    for revX in range(BOARD_SIZE):
+        for revY in range(BOARD_SIZE):
             revealedList[revX][revY] = 1
     render()
 
@@ -65,8 +67,8 @@ def render():
         numsquare = 0;
         for square in line:
             loadImg = images[11]
-            xpos = math.floor(numsquare/50)
-            ypos = math.floor((linestart-100)/50)
+            xpos = math.floor(numsquare / 50)
+            ypos = math.floor((linestart - 100) / 50)
             if revealedList[xpos][ypos] == 1:
                 loadImg = images[square]
             screen.blit(loadImg, Rect(numsquare, linestart, 50, 50))
@@ -84,8 +86,8 @@ def render():
             posy = pos[1]
             if posy > 100:
                 posy = posy - 100
-                posx = min(max(math.floor(posx/50), 0), 12)
-                posy = min(max(math.floor(posy/50), 0), 12)
+                posx = min(max(math.floor(posx / 50), 0), BOARD_SIZE - 1)
+                posy = min(max(math.floor(posy / 50), 0), BOARD_SIZE - 1)
                 revealedList[posx][posy] = 1;
                 if lines[posy][posx] == 9:
                     gameEnd()
